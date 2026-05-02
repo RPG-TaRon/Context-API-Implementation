d694eded5c6ef790ca93b3e2fb839bf6146474d3
Taron Bentley <taronbentley1@gmail.com>
2026-05-01 22:48:32 -0400

    Wrap App component with FilterProvider for filter state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 import { TodoProvider } from "./context/TodoContext";
+import { FilterProvider } from "./context/FilterContext";
+
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-import "./App.css";
 
 function App() {
   return (
     <TodoProvider>
-      <div className="app">
-        <div className="todo-box">
-          <h1>My Todo List</h1>
+      <FilterProvider>
+        <div className="app">
+          <div className="todo-box">
+            <h1>My Todo List</h1>
 
-          <button>Toggle Theme</button>
+            <button>Toggle Theme</button>
 
-          <TodoInput />
+            <TodoInput />
 
-          <div className="filter-row">
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
+            <div className="filter-row">
+              <button>All</button>
+              <button>Active</button>
+              <button>Completed</button>
+            </div>
+
+            <TodoList />
           </div>
-
-          <TodoList />
         </div>
-      </div>
+      </FilterProvider>
     </TodoProvider>
   );
 }
