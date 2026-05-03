307305d24e300a1dd961ff93efd7d445eacdb3f4
Taron Bentley <taronbentley1@gmail.com>
2026-05-03 10:13:25 -0400

    Refactor App component to integrate theme context and update styling for light and dark modes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { TodoProvider } from "./context/TodoContext";
 import { FilterProvider } from "./context/FilterContext";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import FilterButtons from "./components/FilterButtons";
+import ThemeToggleButton from "./components/ThemeToggleButton";
 import "./App.css";
 
 function App() {
@@ -19,12 +20,14 @@ function App() {
 }
 
 function MainApp() {
+  const { theme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <div className="todo-box">
         <h1>My Todo List</h1>
 
-        <button>Toggle Theme</button>
+        <ThemeToggleButton />
 
         <TodoInput />
 
